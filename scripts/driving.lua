9432be8682ef488dc48d98ae116c4cb9da8f522f
Fendi <ltgungen@yahoo.com>
2024-05-13 00:02:36 +0300

    remove unneeded for loop for driving alerts
    Note: This commit mainly changes the indent but gh reads it as a bunch of rewrites.

--- scripts/driving.lua
+++ scripts/driving.lua
@@ -66,170 +66,169 @@ end
 
 --Plays an alert depending on the distance to the entity ahead. Returns whether a larger radius check is needed. Driving proximity alert
 function mod.check_and_play_driving_alert_sound(pindex, tick, mode_in)
-   for pindex, player in pairs(players) do
-      local mode = mode_in or 1
-      local p = game.get_player(pindex)
-      local surf = p.surface
-      --Return in inapplicable cases
-      if p == nil or p.valid == false or p.driving == false or p.vehicle == nil then return false end
-      --Return in vanilla mode
-      if players[pindex].vanilla_mode == true then return end
-      --Return if beeped recently
-      local min_delay = 15
-      if players[pindex].last_driving_alert_tick == nil then
-         players[pindex].last_driving_alert_tick = tick
-         return false
-      end
-      local last_driving_alert_tick = players[pindex].last_driving_alert_tick
-      local time_since = tick - last_driving_alert_tick
-      if last_driving_alert_tick ~= nil and time_since < min_delay then return false end
-      --Scan area "ahead" according to direction
-      local v = p.vehicle
-      local dir = fa_utils.get_heading_value(v)
-      if v.speed < 0 then dir = fa_utils.rotate_180(dir) end
+   --Inits
+   local mode = mode_in or 1
+   local p = game.get_player(pindex)
+   local surf = p.surface
+   --Return in inapplicable cases
+   if p == nil or p.valid == false or p.driving == false or p.vehicle == nil then return false end
+   --Return in vanilla mode
+   if players[pindex].vanilla_mode == true then return end
+   --Return if beeped recently
+   local min_delay = 15
+   if players[pindex].last_driving_alert_tick == nil then
+      players[pindex].last_driving_alert_tick = tick
+      return false
+   end
+   local last_driving_alert_tick = players[pindex].last_driving_alert_tick
+   local time_since = tick - last_driving_alert_tick
+   if last_driving_alert_tick ~= nil and time_since < min_delay then return false end
+   --Scan area "ahead" according to direction
+   local v = p.vehicle
+   local dir = fa_utils.get_heading_value(v)
+   if v.speed < 0 then dir = fa_utils.rotate_180(dir) end
 
-      --Set the trigger distance
-      local trigger = 1
-      if mode == 1 then
-         trigger = 3
-      elseif mode == 2 then
-         trigger = 10
-      elseif mode == 3 then
-         trigger = 25
-      else
-         trigger = 50
-      end
+   --Set the trigger distance
+   local trigger = 1
+   if mode == 1 then
+      trigger = 3
+   elseif mode == 2 then
+      trigger = 10
+   elseif mode == 3 then
+      trigger = 25
+   else
+      trigger = 50
+   end
 
-      --Scan for entities within the radius
-      local ents_around = {}
-      if p.vehicle.type == "car" then
-         local radius = trigger + 5
-         --For cars, exclude anything they cannot collide with
-         ents_around = surf.find_entities_filtered({
-            area = {
-               { v.position.x - radius, v.position.y - radius },
-               { v.position.x + radius, v.position.y + radius },
-            },
-            type = {
-               "resource",
-               "highlight-box",
-               "flying-text",
-               "corpse",
-               "straight-rail",
-               "curved-rail",
-               "rail-signal",
-               "rail-chain-signal",
-               "transport-belt",
-               "underground-belt",
-               "splitter",
-               "item-entity",
-               "pipe",
-               "pipe-to-ground",
-               "inserter",
-               "small-electric-pole",
-               "medium-electric-pole",
-            },
-            invert = true,
-         })
-      elseif p.vehicle.train ~= nil then
-         trigger = trigger * 3
-         local radius = trigger + 5
-         --For trains, search for anything they can collide with
-         ents_around = surf.find_entities_filtered({
-            area = {
-               { v.position.x - radius, v.position.y - radius },
-               { v.position.x + radius, v.position.y + radius },
-            },
-            type = { "locomotive", "cargo-wagon", "fluid-wagon", "artillery-wagon", "character", "car", "unit" },
-            invert = false,
-         })
-      end
+   --Scan for entities within the radius
+   local ents_around = {}
+   if p.vehicle.type == "car" then
+      local radius = trigger + 5
+      --For cars, exclude anything they cannot collide with
+      ents_around = surf.find_entities_filtered({
+         area = {
+            { v.position.x - radius, v.position.y - radius },
+            { v.position.x + radius, v.position.y + radius },
+         },
+         type = {
+            "resource",
+            "highlight-box",
+            "flying-text",
+            "corpse",
+            "straight-rail",
+            "curved-rail",
+            "rail-signal",
+            "rail-chain-signal",
+            "transport-belt",
+            "underground-belt",
+            "splitter",
+            "item-entity",
+            "pipe",
+            "pipe-to-ground",
+            "inserter",
+            "small-electric-pole",
+            "medium-electric-pole",
+         },
+         invert = true,
+      })
+   elseif p.vehicle.train ~= nil then
+      trigger = trigger * 3
+      local radius = trigger + 5
+      --For trains, search for anything they can collide with
+      ents_around = surf.find_entities_filtered({
+         area = {
+            { v.position.x - radius, v.position.y - radius },
+            { v.position.x + radius, v.position.y + radius },
+         },
+         type = { "locomotive", "cargo-wagon", "fluid-wagon", "artillery-wagon", "character", "car", "unit" },
+         invert = false,
+      })
+   end
 
-      --Filter entities by direction
-      local ents_ahead = {}
-      for i, ent in ipairs(ents_around) do
-         local dir_ent = fa_utils.get_direction_biased(ent.position, v.position)
-         if dir_ent == dir then
-            if p.vehicle.type == "car" and ent.unit_number ~= p.vehicle.unit_number then
-               --For cars, take the entity as it is
-               table.insert(ents_ahead, ent)
-            elseif p.vehicle.train ~= nil and ent.unit_number ~= p.vehicle.unit_number then
-               --For trains, the entity must also be near/on rails
-               local ent_straight_rails =
-                  surf.find_entities_filtered({ position = ent.position, radius = 2, type = { "straight-rail" } })
-               local ent_curved_rails =
-                  surf.find_entities_filtered({ position = ent.position, radius = 4, type = { "curved-rail" } })
-               if
-                  (ent_straight_rails ~= nil and #ent_straight_rails > 0)
-                  or (ent_curved_rails ~= nil and #ent_curved_rails > 0)
-               then
-                  if not (ent.train and ent.train.id == v.train.id) then table.insert(ents_ahead, ent) end
-               end
+   --Filter entities by direction
+   local ents_ahead = {}
+   for i, ent in ipairs(ents_around) do
+      local dir_ent = fa_utils.get_direction_biased(ent.position, v.position)
+      if dir_ent == dir then
+         if p.vehicle.type == "car" and ent.unit_number ~= p.vehicle.unit_number then
+            --For cars, take the entity as it is
+            table.insert(ents_ahead, ent)
+         elseif p.vehicle.train ~= nil and ent.unit_number ~= p.vehicle.unit_number then
+            --For trains, the entity must also be near/on rails
+            local ent_straight_rails =
+               surf.find_entities_filtered({ position = ent.position, radius = 2, type = { "straight-rail" } })
+            local ent_curved_rails =
+               surf.find_entities_filtered({ position = ent.position, radius = 4, type = { "curved-rail" } })
+            if
+               (ent_straight_rails ~= nil and #ent_straight_rails > 0)
+               or (ent_curved_rails ~= nil and #ent_curved_rails > 0)
+            then
+               if not (ent.train and ent.train.id == v.train.id) then table.insert(ents_ahead, ent) end
             end
-         elseif
-            mode < 2
-            and util.distance(v.position, ent.position) < 5
-            and (math.abs(dir_ent - dir) == 1 or math.abs(dir_ent - dir) == 7)
-         then
-            --Take very nearby ents at diagonal directions
-            if p.vehicle.type == "car" and ent.unit_number ~= p.vehicle.unit_number then
-               --For cars, take the entity as it is
-               table.insert(ents_ahead, ent)
-            elseif p.vehicle.train ~= nil and ent.unit_number ~= p.vehicle.unit_number then
-               --For trains, the entity must also be near/on rails and not from the same train (if reversing)
-               local ent_straight_rails =
-                  surf.find_entities_filtered({ position = ent.position, radius = 2, type = { "straight-rail" } })
-               local ent_curved_rails =
-                  surf.find_entities_filtered({ position = ent.position, radius = 4, type = { "curved-rail" } })
-               if
-                  (ent_straight_rails ~= nil and #ent_straight_rails > 0)
-                  or (ent_curved_rails ~= nil and #ent_curved_rails > 0)
-               then
-                  if not (ent.train and ent.train.id == v.train.id) then table.insert(ents_ahead, ent) end
-               end
+         end
+      elseif
+         mode < 2
+         and util.distance(v.position, ent.position) < 5
+         and (math.abs(dir_ent - dir) == 1 or math.abs(dir_ent - dir) == 7)
+      then
+         --Take very nearby ents at diagonal directions
+         if p.vehicle.type == "car" and ent.unit_number ~= p.vehicle.unit_number then
+            --For cars, take the entity as it is
+            table.insert(ents_ahead, ent)
+         elseif p.vehicle.train ~= nil and ent.unit_number ~= p.vehicle.unit_number then
+            --For trains, the entity must also be near/on rails and not from the same train (if reversing)
+            local ent_straight_rails =
+               surf.find_entities_filtered({ position = ent.position, radius = 2, type = { "straight-rail" } })
+            local ent_curved_rails =
+               surf.find_entities_filtered({ position = ent.position, radius = 4, type = { "curved-rail" } })
+            if
+               (ent_straight_rails ~= nil and #ent_straight_rails > 0)
+               or (ent_curved_rails ~= nil and #ent_curved_rails > 0)
+            then
+               if not (ent.train and ent.train.id == v.train.id) then table.insert(ents_ahead, ent) end
             end
          end
       end
+   end
 
-      --Skip if nothing is ahead
-      if #ents_ahead == 0 then
-         return true
-      else
-      end
+   --Skip if nothing is ahead
+   if #ents_ahead == 0 then
+      return true
+   else
+   end
 
-      --Get distance to nearest entity ahead
-      local nearest = v.surface.get_closest(v.position, ents_ahead)
-      if nearest == nil then
-         --Skip if nearest does not exist
-         return true
-      end
-      local edge_dist = util.distance(v.position, nearest.position) - 1 / 4 * (nearest.tile_width + nearest.tile_height)
+   --Get distance to nearest entity ahead
+   local nearest = v.surface.get_closest(v.position, ents_ahead)
+   if nearest == nil then
+      --Skip if nearest does not exist
+      return true
+   end
+   local edge_dist = util.distance(v.position, nearest.position) - 1 / 4 * (nearest.tile_width + nearest.tile_height)
+   rendering.draw_circle({
+      color = { 0.8, 0.8, 0.8 },
+      radius = 2,
+      width = 2,
+      target = nearest,
+      surface = p.surface,
+      time_to_live = 15,
+   })
+
+   --Beep
+   if edge_dist < trigger then
+      p.play_sound({ path = "player-bump-stuck-alert" })
+      players[pindex].last_driving_alert_tick = last_driving_alert_tick
+      players[pindex].last_driving_alert_ent = nearest
       rendering.draw_circle({
-         color = { 0.8, 0.8, 0.8 },
+         color = { 1.0, 0.4, 0.2 },
          radius = 2,
          width = 2,
          target = nearest,
          surface = p.surface,
          time_to_live = 15,
       })
-
-      --Beep
-      if edge_dist < trigger then
-         p.play_sound({ path = "player-bump-stuck-alert" })
-         players[pindex].last_driving_alert_tick = last_driving_alert_tick
-         players[pindex].last_driving_alert_ent = nearest
-         rendering.draw_circle({
-            color = { 1.0, 0.4, 0.2 },
-            radius = 2,
-            width = 2,
-            target = nearest,
-            surface = p.surface,
-            time_to_live = 15,
-         })
-         return false
-      end
-      return true
+      return false
    end
+   return true
 end
 
 function mod.stop_vehicle(pindex)
