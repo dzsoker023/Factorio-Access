e981d2a58114aec264e892ff769d73ce00189efc
Austin Hicks <ahicks@ahicks.io>
2024-10-16 16:47:50 -0700

    Fix: don't report that pumpjakcs and other fluid-only drills have a drop position

--- scripts/fa-info.lua
+++ scripts/fa-info.lua
@@ -1497,8 +1497,18 @@ function mod.read_selected_entity_status(pindex)
 end
 
 function mod.cursor_is_at_mining_drill_output_part(pindex, ent)
+   -- From docs: "If the drill does not produce any solid items but uses a
+   -- fluidbox output instead (e.g. pumpjacks), a vector of {0,0} disables the
+   -- yellow arrow alt-mode indicator for the placed item location."
+   --
+   -- What we actually get is that the drop position is the center of the entity
+   -- exactly.
+   local drop_pos = ent.drop_position
+   local ent_pos = ent.position
+   if ent_pos.x == drop_pos.x and ent_pos.y == drop_pos.y then return false end
+
    local dir = ent.direction
-   local correct_pos = fa_utils.offset_position(ent.drop_position, fa_utils.rotate_180(dir), 1)
+   local correct_pos = fa_utils.offset_position(drop_pos, fa_utils.rotate_180(dir), 1)
    return util.distance(correct_pos, players[pindex].cursor_pos) < 0.6
 end
 
